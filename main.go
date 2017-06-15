6179b35d9fef61b0c79fc1d839eecb137bbeb84d
Marco Younan <xmarcoied@gmail.com>
2017-06-15 18:12:52 +0100

    extracting read/write fn to helper.go

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-    "os"
-    "bufio"
-    "bytes"
-    "io"
-    "fmt"
+
+    //"fmt"
     "net/http"
     "strings"
     "log"
@@ -20,7 +17,6 @@ type Page struct{
 }
 func homepage(w http.ResponseWriter, r *http.Request) {
     data, _ := readLines("in.txt") 
-    //data := "Marco"
     t, _ := template.ParseFiles("templates/home.html")
     p := Page{Body : data}
     t.Execute(w , p)
@@ -33,59 +29,6 @@ func statusHandler(w http.ResponseWriter, r *http.Request) {
 func status_ascHandler(w http.ResponseWriter, r *http.Request) {
     http.ServeFile(w, r, "static/status.asc")
     log.Println("status.asc")
-}
-
-func readLines(path string) (lines []string, err error) {
-    var (
-        file *os.File
-        part []byte
-        prefix bool
-    )
-    if file, err = os.Open(path); err != nil {
-        return
-    }
-    defer file.Close()
-
-    reader := bufio.NewReader(file)
-    buffer := bytes.NewBuffer(make([]byte, 0))
-    for {
-        if part, prefix, err = reader.ReadLine(); err != nil {
-            break
-        }
-        buffer.Write(part)
-        if !prefix {
-            lines = append(lines, buffer.String())
-            buffer.Reset()
-        }
-    }
-    if err == io.EOF {
-        err = nil
-    }
-    return
-}
-
-func writeLines(lines []string, path string) (err error) {
-    var (
-        file *os.File
-    )
-
-    if file, err = os.Create(path); err != nil {
-        return
-    }
-    defer file.Close()
-
-    //writer := bufio.NewWriter(file)
-    for _,item := range lines {
-        //fmt.Println(item)
-        _, err := file.WriteString(strings.TrimSpace(item) + "\n"); 
-        //file.Write([]byte(item)); 
-        if err != nil {
-            //fmt.Println("debug")
-            fmt.Println(err)
-            break
-        }
-    }
-    return
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
