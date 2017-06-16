f5b260dcaadb24a4a684e38d0c90a6f95a2f33bc
Marco Younan <xmarcoied@gmail.com>
2017-06-16 18:02:50 +0100

    Basic Database connection

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+  	"database/sql"
+  	_ "github.com/lib/pq"
     
     "updater/router"
 )
 
-
 func main() {
-	
-	server.StartServer()    
+	// TODO : Move this to Model/database package
+  	psqlInfo := "host=localhost dbname=updater user=postgres password=postgres sslmode=disable"
+    db, _ := sql.Open("postgres", psqlInfo)
+    db.Close()
+    server.StartServer()
 }
