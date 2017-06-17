12b522e9a55209dc7a8b7e18c23e5bd6c84f04e5
Marco Younan <xmarcoied@gmail.com>
2017-06-17 01:56:57 +0100

    basic gorm model

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
-  	"database/sql"
-  	_ "github.com/lib/pq"
+	"github.com/jinzhu/gorm"
+       _"github.com/lib/pq"
     
-    "updater/router"
+	"updater/router"
 )
+type Update_Request struct{
+	gorm.Model
+	ID          	uint   `gorm:"primary_key"`
+	Os 	 	string 	   
+	Os_ver 		string
+	Os_arch 	string 
+	Vlc_ver 	string
+}
 
 func main() {
 	// TODO : Move this to Model/database package
-  	psqlInfo := "host=localhost dbname=updater user=postgres password=postgres sslmode=disable"
-    db, _ := sql.Open("postgres", psqlInfo)
-    db.Close()
-    server.StartServer()
+	// TODO : Move the psqlinfo to config
+	psqlInfo := "host=localhost dbname=updater user=postgres password=postgres sslmode=disable"
+	db , _ := gorm.Open("postgres" , psqlInfo)
+	defer db.Close()
+  	db.AutoMigrate(&Update_Request{})
+  	db.LogMode(true)
+
+    	server.StartServer()
 }
