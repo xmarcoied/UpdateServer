1bb8420de543a3676089e151269174e2e7d7cc43
Marco Younan <xmarcoied@gmail.com>
2017-06-20 21:33:50 +0100

    basic Gin routing

--- main.go
+++ main.go
@@ -1,49 +1,39 @@
 package main 
 import (
     	"log"
-    	"encoding/json"
     	"net/http"
-     	//"github.com/gin-gonic/gin"
+     	"github.com/gin-gonic/gin"
     	"updater/model"
 )
-// TODO : Use Gin package 
+// In Process : Use Gin package 
 // Global variables are bad 
 var db model.Impl
 
 func main(){
 		db.ConnectDB()
 		defer db.CloseDB()
+        router := gin.Default();
 
-	    http.HandleFunc("/showoff" , showoff)
-        http.HandleFunc("/update", update) 
-        http.HandleFunc("/vlc/status", statusHandler)
-        http.HandleFunc("/vlc/status.asc", status_ascHandler)
-        log.Fatal(http.ListenAndServe(":80", nil))
+        // TODO : Create a controller for the status managment
+        router.StaticFile("status" , "./client/static/status")
+        router.StaticFile("status.asc" , "./client/static/status.asc")
+	    router.GET("/showoff" , showoff)
+        router.POST("/update", update)
+        router.Run(":80")
 }
 
-// TODO : Create a controller for the status managment
-func statusHandler(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "client/static/status")
-        log.Println("status")
-}
-func status_ascHandler(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "client/static/status.asc")
-        log.Println("status.asc")
-}
-func showoff(w http.ResponseWriter, r *http.Request) {
+func showoff(c *gin.Context) {
     	var requests []model.Update_Request
         requests = db.AllRequests(requests)
+        c.String(http.StatusOK, "Hello")
         log.Println(requests)
 }
 
-func update(w http.ResponseWriter, r *http.Request) {
+func update(c *gin.Context) {
         // Assuming VLC is sending JSON
         var request model.Update_Request
-        err := json.NewDecoder(r.Body).Decode(&request)
-        if err != nil {
-            http.Error(w, err.Error(), 400)
-            return
-        }
+        c.BindJSON(&request)
+        log.Println(request)
         // TODO : DB Model API
         // FIXME : initiate the DB once and pass it everywhere
         db.NewRequest(request)
