35a46d651aff15ec381dfd10f264bdd2a04a132d
Marco Younan <xmarcoied@gmail.com>
2017-08-26 23:19:31 +0200

    Releases: minor fixes with releases - return release_active to inactive with duplicate release - set confirmation before duplicating releases

--- http/release.go
+++ http/release.go
@@ -232,6 +232,8 @@ func DuplicateRelease(c *gin.Context) {
 	release := core.GetRelease(c.Param("id"))
 	// setting the release_id to zero to avoid violating releases_pkey
 	release.ID = 0
+	// set the release_status to inactive
+	release.Active = false
 	core.NewRelease(&release)
 	c.Redirect(http.StatusMovedPermanently, "/admin/dashboard/release/"+strconv.Itoa(int(release.ID)))
 }
